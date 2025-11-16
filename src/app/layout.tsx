f0d0139994e8cbb3a4a7e92e431dd5dd763f6b3b
yourcoderomar <omarmostafamohie12@gmail.com>
2025-11-16 20:42:38 +0200

    body white

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={inter.variable}>
-      <body className="antialiased">
+      <body className="antialiased bg-white">
         {children}
       </body>
     </html>
